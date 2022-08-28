f5a9ba8867a5f85521966dac264147ce1cb201b8
PrattayDhar <prattaygub@gmail.com>
2022-08-28 20:37:20 +0100

    practice

--- api.js
+++ api.js
@@ -28,10 +28,52 @@ const shopjson=JSON.stringify(shop);
 //     .then(json => console.log(json));
 
 
-const url = 'https://jsonplaceholder.typicode.com/todos/1'
+// const url = 'https://jsonplaceholder.typicode.com/todos/1'
 
-function loaddata(){
-    fetch(url)
-    .then (response =>response.json())
-    .then(json=>console.log(json))
+// function loaddata(){
+//     fetch(url)
+//     .then (response =>response.json())
+//     .then(json=>console.log(json))
+// }
+
+// function loaddynamic(){
+//     fetch('https://jsonplaceholder.typicode.com/users')
+//     .then(res => res.json())
+//     .then(data => displaydata(data))
+
+// }
+// function displaydata(data){
+//     const ul = document.getElementById('list');
+//     for(const user of data){
+//         console.log(user.name);
+//         const li =document.createElement('li');
+//         li.innerText=user.name;
+//         ul.appendChild(li);
+
+//     }
+// }
+
+
+
+function loadpost(){
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(res=>res.json())
+    .then(data => displaypost(data) )
 }
+
+function displaypost(posts){
+    const Postcoinatiner=document.getElementById('hudai')
+    for(const post of posts){
+    const postdiv=document.createElement('div');
+    console.log(post);
+    postdiv.innerHTML =`
+        <h4> user-${post.userId}</h4>
+        <h5> post-${post.title}</h5>
+        <p> tirtel-</p>
+    `;
+    Postcoinatiner.appendChild(postdiv);
+    
+    }
+}
+a
+loadpost()
